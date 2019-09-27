d7767761752b5ba2bf6173dd78ddaa3006dacb81
kozi <pkozubek92@gmail.com>
2019-09-27 20:51:24 +0200

    Updates

---
+++ precache-manifest.6c6c14fe69ae0276218e7371a1bb8e3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323ed9bf893377ceb319c70a32cecfe4",
+    "url": "/star-wars-characters/index.html"
+  },
+  {
+    "revision": "1323dc6120838d490f16",
+    "url": "/star-wars-characters/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "27410a8c27fe75fb13b0",
+    "url": "/star-wars-characters/static/js/2.496acd04.chunk.js"
+  },
+  {
+    "revision": "1323dc6120838d490f16",
+    "url": "/star-wars-characters/static/js/main.edca155d.chunk.js"
+  },
+  {
+    "revision": "21b61da4ea008d654c9b",
+    "url": "/star-wars-characters/static/js/runtime-main.e8a25fe6.js"
+  },
+  {
+    "revision": "8644e275184c838767566c3d38d9bce2",
+    "url": "/star-wars-characters/static/media/space.8644e275.png"
+  }
+]);
